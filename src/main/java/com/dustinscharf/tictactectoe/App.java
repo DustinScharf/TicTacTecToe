761b26ae17e6a8a1a013815bdf74667e7c92f7ef
DustinScharf <d.scharf.2002@gmail.com>
2021-08-02 03:02:59 +0200

    Initial commit

---
+++ src/main/java/com/dustinscharf/tictactectoe/App.java
@@ -0,0 +1,31 @@
+package com.dustinscharf.tictactectoe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/App.fxml"));
+        Parent root = fxmlLoader.load();
+
+        // Controller controller = fxmlLoader.getController();
+
+        Scene scene = new Scene(root);
+        // scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
+
+        primaryStage.setTitle("TicTacTecToe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
