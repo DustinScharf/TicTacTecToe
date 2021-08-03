6acd17a2f34a8b392f7e2fe31882550817bb28b1
DustinScharf <d.scharf.2002@gmail.com>
2021-08-03 03:30:42 +0200

    Added JavaFX attributes to classes

--- src/main/java/com/dustinscharf/tictactectoe/Controller.java
+++ src/main/java/com/dustinscharf/tictactectoe/Controller.java
@@ -1,4 +1,26 @@
 package com.dustinscharf.tictactectoe;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+
 public class Controller {
+    @FXML
+    private GridPane gridPaneBoardFieldButtons;
+
+    @FXML
+    private VBox vBoxPlayer1PlacerButtons;
+
+    @FXML
+    private VBox vBoxPlayer2PlacerButtons;
+
+    public void testController() {
+        ObservableList<Node> boardFieldButtons = this.gridPaneBoardFieldButtons.getChildren();
+        boardFieldButtons.forEach(System.out::println);
+    }
 }
