f484280e5834d407ef0ab396b6fe6cde6bb9d4bc
DustinScharf <d.scharf.2002@gmail.com>
2021-08-04 16:38:40 +0200

    Buttons are now pseudo buttons (now canvas to draw on)

--- src/main/java/com/dustinscharf/tictactectoe/Controller.java
+++ src/main/java/com/dustinscharf/tictactectoe/Controller.java
@@ -3,11 +3,12 @@ package com.dustinscharf.tictactectoe;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-
-import java.util.List;
+import javafx.scene.paint.Color;
 
 public class Controller {
     @FXML
@@ -18,6 +19,24 @@ public class Controller {
 
     @FXML
     private VBox vBoxPlayer2PlacerButtons;
+
+    private Game controlledGame;
+
+    public void receiveGame(Game game) {
+        this.controlledGame = game;
+    }
+
+    public void boardClicked(MouseEvent mouseEvent) {
+        System.out.println("Clicked!");
+        Node clickedNode = mouseEvent.getPickResult().getIntersectedNode();
+        System.out.println(clickedNode);
+
+        GraphicsContext graphicsContext = ((Canvas) clickedNode).getGraphicsContext2D();
+        graphicsContext.setFill(Color.GREEN);
+        graphicsContext.setStroke(Color.BLUE);
+        graphicsContext.setLineWidth(5);
+        graphicsContext.strokeLine(40, 10, 10, 40);
+    }
 
     public void testController() {
         ObservableList<Node> boardFieldButtons = this.gridPaneBoardFieldButtons.getChildren();
