5941ffeb975831204ba2cd29fc416eac53ac61b4
DustinScharf <d.scharf.2002@gmail.com>
2021-08-16 13:48:56 +0200

    Client can now send cords of the clicked field

--- src/main/java/com/dustinscharf/tictactectoe/network/client/Client.java
+++ src/main/java/com/dustinscharf/tictactectoe/network/client/Client.java
@@ -19,12 +19,12 @@ public class Client {
 
     public Client() {
         try {
-            this.socket = new Socket("localhost", Server.STANDARD_PORT);
+            this.socket = new Socket("localhost"/*TODO*/, Server.STANDARD_PORT);
 
             this.dataInputStream = new DataInputStream(this.socket.getInputStream());
             this.dataOutputStream = new DataOutputStream(this.socket.getOutputStream());
         } catch (IOException e) {
-            System.err.println("Could not init client");
+            System.err.println("NETWORK ERROR: CLIENT COULD NOT CONNECT TO SERVER");
             System.exit(1);
         }
 
@@ -32,20 +32,18 @@ public class Client {
     }
 
     private void startClientLoop() {
-        new Thread(this::sendMessage).start();
+//        new Thread(this::sendMessage).start();
         new Thread(this::receiveMessage).start();
     }
 
-    private void sendMessage() {
-        while (true) {
-            this.clientText = JOptionPane.showInputDialog("Message");
-            try {
-                this.dataOutputStream.writeUTF(this.clientText);
-                this.dataOutputStream.flush();
-                System.out.println("OUT: " + this.clientText);
-            } catch (IOException e) {
-                System.err.println("Message could not send error");
-            }
+    private void sendMessage(String message) {
+        this.clientText = message;
+        try {
+            this.dataOutputStream.writeUTF(this.clientText);
+            this.dataOutputStream.flush();
+            System.out.println("OUT: " + this.clientText);
+        } catch (IOException e) {
+            System.err.println("NETWORK ERROR: CLIENT MESSAGE NOT SEND RIGHT");
         }
     }
 
@@ -55,8 +53,12 @@ public class Client {
                 this.serverText = this.dataInputStream.readUTF();
                 System.out.println("IN: " + this.serverText);
             } catch (IOException e) {
-                System.err.println("Message not received error");
+                System.err.println("NETWORK ERROR: MESSAGE NOT RECEIVED RIGHT BY CLIENT");
             }
         }
+    }
+
+    public void sendClickedFieldByCords(int row, int col) {
+        this.sendMessage("F" + row + "" + col);
     }
 }
