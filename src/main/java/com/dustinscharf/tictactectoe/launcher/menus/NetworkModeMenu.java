336b2fb5a3123846451498dcc82172ef38cde9e2
DustinScharf <d.scharf.2002@gmail.com>
2021-08-23 15:44:16 +0200

    Added back button for the network menu

--- src/main/java/com/dustinscharf/tictactectoe/launcher/menus/NetworkModeMenu.java
+++ src/main/java/com/dustinscharf/tictactectoe/launcher/menus/NetworkModeMenu.java
@@ -39,4 +39,9 @@ public class NetworkModeMenu {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         new NetworkModeJoinMenu().show(stage, this.gameLauncher);
     }
+
+    public void backToMainMenu(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(new FXMLLoader(getClass().getResource("/StartMenu.fxml")).load()));
+    }
 }
